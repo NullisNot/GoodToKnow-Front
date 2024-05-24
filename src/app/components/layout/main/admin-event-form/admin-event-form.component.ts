27db5dd8073c48fad77d489375c8d0bbb71b560b
maguilerafr <lolo.aguilera.98@gmail.com>
2024-05-24 17:47:38 +0200

    Create and elaborate: Event Form Component + Create: AdminPrincipalCalendar like a components page

---
+++ src/app/components/layout/main/admin-event-form/admin-event-form.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-event-form',
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
+  templateUrl: './admin-event-form.component.html',
+  styleUrls: ['./admin-event-form.component.css'],
+})
+export class AdminEventFormComponent {
+  'subject': string;
+  'teacher': string;
+  'startsAt': Date;
+  'finishesAt': Date;
+  'building': string;
+  'classroom': string;
+  'link': string;
+  'comments': string;
+  'notification': boolean;
+
+  submitForm() {
+    console.log('Datos del formulario:', {
+      subject: this.subject,
+      teacher: this.teacher,
+      startsAt: this.startsAt,
+      finishesAt: this.finishesAt,
+      building: this.building,
+      classroom: this.classroom,
+      link: this.link,
+      comments: this.comments,
+      notification: this.notification,
+    });
+  }
+}
