ae8e63d5a9988f43a4f2abb9bd63c491f3c1a83f
maguilerafr <lolo.aguilera.98@gmail.com>
2024-06-07 18:01:24 +0200

    files structure

---
+++ src/app/components/Pages/admin-information/admin-information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-information.component.html',
+  styleUrl: './admin-information.component.css'
+})
+export class AdminInformationComponent {
+
+}
---
+++ src/app/components/Pages/admin-notification/admin-notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-notification',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-notification.component.html',
+  styleUrl: './admin-notification.component.css'
+})
+export class AdminNotificationComponent {
+
+}
