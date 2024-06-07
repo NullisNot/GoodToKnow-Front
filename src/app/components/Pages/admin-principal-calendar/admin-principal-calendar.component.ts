178e5bb5a7d327bc9662c8eee72ddd19f832d3dc
maguilerafr <lolo.aguilera.98@gmail.com>
2024-06-07 19:46:23 +0200

    fix sintax error

--- src/app/components/Pages/admin-principal-calendar/admin-principal-calendar.component.ts
+++ src/app/components/Pages/admin-principal-calendar/admin-principal-calendar.component.ts
@@ -3,15 +3,21 @@ import { AdminEventFormComponent } from '../../layout/main/admin-event-form/admi
 import { CommonModule } from '@angular/common';
 import { NavComponent } from '../../layout/header/nav/nav.component';
 import { CalendarComponent } from '../../layout/main/calendar/calendar.component';
-import { FooterComponent } from "../../layout/footer/footer.component";
+import { FooterComponent } from '../../layout/footer/footer.component';
+import { EventsService } from '../../../services/events.service';
 
 @Component({
-
-    selector: 'app-admin-principal-calendar',
-    standalone: true,
-    templateUrl: './admin-principal-calendar.component.html',
-    styleUrl: './admin-principal-calendar.component.css',
-    providers: [EventsService],
-    imports: [AdminEventFormComponent, CommonModule, CalendarComponent, FooterComponent, NavComponent]
+  selector: 'app-admin-principal-calendar',
+  standalone: true,
+  templateUrl: './admin-principal-calendar.component.html',
+  styleUrl: './admin-principal-calendar.component.css',
+  providers: [EventsService],
+  imports: [
+    AdminEventFormComponent,
+    CommonModule,
+    CalendarComponent,
+    FooterComponent,
+    NavComponent,
+  ],
 })
 export class AdminPrincipalCalendarComponent {}
