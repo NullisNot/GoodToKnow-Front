07b344f31a5dd4a06eaf6b4bc506cd65597d30a7
maguilerafr <lolo.aguilera.98@gmail.com>
2024-06-13 13:24:15 +0200

    Issue complete. CSS styles and Deleted button finished

--- src/app/components/Pages/admin-principal-calendar/admin-principal-calendar.component.ts
+++ src/app/components/Pages/admin-principal-calendar/admin-principal-calendar.component.ts
@@ -5,27 +5,27 @@ import { NavComponent } from '../../layout/header/nav/nav.component';
 import { CalendarComponent } from '../../layout/main/calendar/calendar.component';
 import { FooterComponent } from '../../layout/footer/footer.component';
 import { EventsService } from '../../../services/events.service';
-import { AdminEventEditComponent } from "../../layout/main/admin-edit-event-form/admin-edit-event-form.component";
+import { AdminEventEditComponent } from '../../layout/main/admin-edit-event-form/admin-edit-event-form.component';
 
 @Component({
-    selector: 'app-admin-principal-calendar',
-    standalone: true,
-    templateUrl: './admin-principal-calendar.component.html',
-    styleUrl: './admin-principal-calendar.component.css',
-    providers: [EventsService],
-    imports: [
-        AdminEventFormComponent,
-        CommonModule,
-        CalendarComponent,
-        FooterComponent,
-        NavComponent,
-        AdminEventEditComponent
-    ]
+  selector: 'app-admin-principal-calendar',
+  standalone: true,
+  templateUrl: './admin-principal-calendar.component.html',
+  styleUrl: './admin-principal-calendar.component.css',
+  providers: [EventsService],
+  imports: [
+    CommonModule,
+    CalendarComponent,
+    FooterComponent,
+    NavComponent,
+    AdminEventEditComponent,
+    AdminEventFormComponent,
+  ],
 })
 export class AdminPrincipalCalendarComponent {
-    selectedEventDay?: string;
+  selectedEventDay?: string;
 
-    onEventSelected(day: string){
-        this.selectedEventDay = day
-    }
+  onEventSelected(day: string) {
+    this.selectedEventDay = day;
+  }
 }
