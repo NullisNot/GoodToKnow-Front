7d3932f22495f56aa3459aa4f9b6a570dc5aa77d
ErikaVorozco <155074319+ErikaVorozco@users.noreply.github.com>
2024-06-17 20:11:54 +0200

    duplicación footer y nav

--- src/app/components/Pages/admin-create-events/admin-create-events.component.ts
+++ src/app/components/Pages/admin-create-events/admin-create-events.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { BlockEventsComponent } from '../../layout/main/block-events/block-events.component';
-import { FooterComponent } from '../../layout/footer/footer.component';
-import { NavComponent } from '../../layout/header/nav/nav.component';
+
 
 @Component({
   selector: 'app-admin-create-events',
   standalone: true,
-  imports: [BlockEventsComponent, FooterComponent, NavComponent],
+  imports: [BlockEventsComponent],
   templateUrl: './admin-create-events.component.html',
   styleUrl: './admin-create-events.component.css',
 })
